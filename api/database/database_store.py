00954a4c47b763ff0c0e035e39201dbb0f950c74
juhitripathi <jt866@scarletmail.rutgers.edu>
2019-08-21 20:12:42 -0400

    feat:add database functionalities to create, search, modify and get database length

--- api/database/database_store.py
+++ api/database/database_store.py
@@ -1,16 +1,49 @@
 class DatabaseStore:
 
     def __init__():
-        self.database = []
+        self.database = {}
 
-    def create_object():
-        raise NotImplementedError()
+    def create_collection(self, collection_name):
+        if collection_name in database:
+            return 'Collection already exists. Please select different name.'
+        else:
+            database[collection_name] = []
+            return 'Collection successfully added.'
 
-    def search_store():
-        raise NotImplementedError()
+    def create_document(self, collection_name, document):
+        if not collection_name in database:
+            return 'Collection does not exist.'
+        else:
+            database[collection_name].append(document)
+            return 'Document successfully added.'
 
-    def modify_object():
-        raise NotImplementedError()
+    def search_store(self, collection_name, search_key, search_value):
+        if not collection_name in database:
+            return 'Collection does not exist.'
+        else:
+            search_result = [document for document in database[collection_name] if search_key in document and search_value == document[search_key]]
+            return search_result
 
-    def get_store_size():
-        raise NotImplementedError()
+    def modify_object(self, collection_name, search_key, old_value, new_value):
+        if not collection_name in database:
+            return 'Collection does not exist.'
+        else:
+            for i in database[collection_name]
+                if search_key in i and old_value == i[search_key]:
+                    i[search_key] = new_value
+                    continue
+        return 'Document successfully updated.'
+
+    def get_collection_size(self, collection_name):
+        if not collection_name in database:
+            return 'Collection does not exist.'
+        else:
+            return len(database[collection_name])
+
+    def get_document_size(self, collection_name, search_key, search_value):
+        if not collection_name in database:
+            return 'Collection does not exist.'
+        else:
+            for i in database[collection_name]
+                if search_key in i and search_value == i[search_key]:
+                    return len(i)
